990cbe9ac3f5d2231c34ec8283cea6709fab36f0
shahstudios <sshahcode@gmail.com>
2021-06-17 19:47:42 -0400

    setup increment button

---
+++ project-02/js/index.js
@@ -0,0 +1,28 @@
+/* create variables to store data
+let count = 0
+use console for debugging
+console.log(count)
+ */
+
+// 1. Create a variable, myAge, and set its value to your age
+// let myAge = 23
+// 2. Log the myAge variable to the console
+// console.log(myAge)
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+// let myAge = 23
+// let humanDogRatio = 7
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge);
+
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+let bonusPoints = 50
+bonusPoints = bonusPoints + 50
+bonusPoints = bonusPoints - 75
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
