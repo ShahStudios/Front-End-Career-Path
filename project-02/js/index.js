af32dfedcd8b5ce019fda4b04b0fc5e1ab80185e
shahstudios <sshahcode@gmail.com>
2021-06-17 20:32:13 -0400

    create increment function & its logic

--- project-02/js/index.js
+++ project-02/js/index.js
@@ -1,28 +1,11 @@
-/* create variables to store data
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
 let count = 0
-use console for debugging
-console.log(count)
- */
 
-// 1. Create a variable, myAge, and set its value to your age
-// let myAge = 23
-// 2. Log the myAge variable to the console
-// console.log(myAge)
-
-// 1. Create two variables, myAge and humanDogRatio
-// 2. Multiply the two together and store the result in myDogAge 
-// 3. Log myDogAge to the console
-// let myAge = 23
-// let humanDogRatio = 7
-// let myDogAge = myAge * humanDogRatio
-// console.log(myDogAge);
-
-
-// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
-// Decrease it down to 25, and then finally increase it to 70
-// Console.log the value after each step
-let bonusPoints = 50
-bonusPoints = bonusPoints + 50
-bonusPoints = bonusPoints - 75
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
+function increment() {
+    count++
+    console.log(count)
+}
