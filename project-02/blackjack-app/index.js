ca4debbc507624fa4db969591d64a73df64bc55b
shahstudios <sshahcode@gmail.com>
2021-06-19 19:31:13 -0400

    get elements by id and add more to the startGame function

--- project-02/blackjack-app/index.js
+++ project-02/blackjack-app/index.js
@@ -1,4 +1,6 @@
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 let firstCard = 10
 let secondCard = 4
@@ -6,18 +8,25 @@ let sum = firstCard + secondCard
 
 let hasBlackJack = false
 let isAlive = true
+
 let message = ""
 
-
 function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
     messageEl.textContent = message
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
 }
