5276b62316af5d3dd9159eb28077f7ab647e6b88
shahstudios <sshahcode@gmail.com>
2021-06-21 18:30:31 -0400

    deploy extension and save to local storage

--- module-03/index.js
+++ module-03/index.js
@@ -4,10 +4,12 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function () {
+inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
+
 })
 
 function renderLeads() {
