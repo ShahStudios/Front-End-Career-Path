8a2e6c1e2688ba55dd0c0b041ae53e581f7ae4f3
shahstudios <sshahcode@gmail.com>
2021-06-21 19:49:14 -0400

    create/style delete button for app

--- module-03/index.js
+++ module-03/index.js
@@ -3,13 +3,27 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
-
 })
 
 function renderLeads() {
