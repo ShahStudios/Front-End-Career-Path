8096a93c2b14681389c301d0d96ae8380c316709
shahstudios <sshahcode@gmail.com>
2021-06-21 15:40:47 -0400

    create a working prototype of the extension

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 let myLeads = []
-let inputEl = document.getElementById("input-el")
 
-let inputBtn = document.getElementById("input-btn")
+const  inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked!")
+    myLeads.push(inputEl.value)
+    renderLeads()
+    inputEl.value = ""
 })
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems  
+}
