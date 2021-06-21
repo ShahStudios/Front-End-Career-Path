87dc0b7c3f80883e558f69f11ea4200893b551ab
shahstudios <sshahcode@gmail.com>
2021-06-21 16:14:40 -0400

    stylee the lead links

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 let myLeads = []
 
-const  inputEl = document.getElementById("input-el")
+const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function() {
+inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
-    renderLeads()
     inputEl.value = ""
+    renderLeads()
 })
 
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += `
+        <li>
+        <a target='_blank' href='${myLeads[i]}'>
+            ${myLeads[i]}
+        </a>
+    </li>
+        `
     }
-    ulEl.innerHTML = listItems  
+    ulEl.innerHTML = listItems
 }
