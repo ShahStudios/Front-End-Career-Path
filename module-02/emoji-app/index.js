5005b23defb943c7defb6b0c003b3c2f80514d52
shahstudios <sshahcode@gmail.com>
2021-06-23 20:46:29 -0400

    render emojios to app

---
+++ module-02/emoji-app/index.js
@@ -0,0 +1,8 @@
+const emojiContainer = document.getElementById("emojiContainer")
+// console.log(emojiContainer)
+
+const myEmojis = ["👨🏽‍💻", "🎮", "🍕"]
+
+for (let i=0; i<myEmojis.length; i++) {
+    console.log(myEmojis)
+}
