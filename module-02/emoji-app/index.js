309447daeb3100020d4168de5a785b8f7157aa94
shahstudios <sshahcode@gmail.com>
2021-06-23 20:58:58 -0400

    get btn from html and add functionality to it

--- module-02/emoji-app/index.js
+++ module-02/emoji-app/index.js
@@ -1,8 +1,15 @@
-const emojiContainer = document.getElementById("emojiContainer")
-// console.log(emojiContainer)
+const emojiContainer = document.getElementById("emoji-container")
+const pushBtn = document.getElementById("push-btn")
 
 const myEmojis = ["👨🏽‍💻", "🎮", "🍕"]
 
-for (let i=0; i<myEmojis.length; i++) {
-    console.log(myEmojis)
+for (let i = 0; i < myEmojis.length; i++) {
+    emojiContainer.innerHTML += `<span>${myEmojis[i]}</span>`
 }
+
+const pushBtn = document.getElementById("push-btn")
+pushBtn.addEventListener("click", function() {
+    // console.log("click")
+    const emojiInput = document.getElementById("emoji-input")
+    console.log(emojiInput.value)
+})
