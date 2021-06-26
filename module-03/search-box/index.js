7028f8c0601200abf2ea6fa6260fb110427f8296
shahstudios <sshahcode@gmail.com>
2021-06-26 18:47:15 -0400

    Run loop and iterate thru the query

--- module-03/search-box/index.js
+++ module-03/search-box/index.js
@@ -1,7 +1,11 @@
-const searchInput = document.getElementById("searchInput")
-searchInput.addEventListener("keyup", function(event) {
-    let searchQuery = event.target.value.toLowerCase()
-    console.log(searchQuery)
-    let allNamesDOMCollection = document.getElementsByClassName('name');
-    console.log(allNamesDOMCollection[1].textContent);
-})
+const searchInput = document.getElementById("searchInput");
+
+searchInput.addEventListener("keyup", function (event) {
+    let searchQuery = event.target.value.toLowerCase();
+    let allNamesDOMCollection = document.getElementsByClassName("name");
+
+    for (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
+        const currentName = allNamesDOMCollection[counter].textContent;
+        console.log(currentName);
+    }
+});
