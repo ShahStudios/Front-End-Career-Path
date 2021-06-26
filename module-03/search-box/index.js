f870a1fef8245362c8782693ea4cbce9c44de1a7
shahstudios <sshahcode@gmail.com>
2021-06-26 18:35:57 -0400

    get html collection

--- module-03/search-box/index.js
+++ module-03/search-box/index.js
@@ -1,5 +1,7 @@
-const usernameInput = document.getElementById("usernameInput")
-usernameInput.addEventListener("keyup", function() {
-    let username = event.target.value.toLowerCase()
-    console.log(username)
+const searchInput = document.getElementById("searchInput")
+searchInput.addEventListener("keyup", function(event) {
+    let searchQuery = event.target.value.toLowerCase()
+    console.log(searchQuery)
+    let allNamesDOMCollection = document.getElementsByClassName('name');
+    console.log(allNamesDOMCollection[1].textContent);
 })
