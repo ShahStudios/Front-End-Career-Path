5881a5248a91bc49132edf7f906085e75018d31d
shahstudios <sshahcode@gmail.com>
2021-06-28 19:37:36 -0400

    store references to dom nodes

--- module-04/dice-game/index.js
+++ module-04/dice-game/index.js
@@ -1,3 +1,14 @@
+const player1Dice = document.getElementById("player1Dice")
+const player2Dice = document.getElementById("player2Dice")
+const player1Scoreboard = document.getElementById("player1Scoreboard")
+const player2Scoreboard = document.getElementById("player2Scoreboard")
+const message = document.getElementById("message")
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
+
 let player1Score = 0
 let player2Score = 0
-let player1Turn = true
+let player1Turn = true 
+
+
+// 
